996491ac5d59e424c8c003adc26974707f249366
asimwe1 <landryasimwe@gmail.com>
2024-05-17 16:21:55 +0200

    shop online with our team

---
+++ admin/includes/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>All rights reserved</b>
+    </div>
+    <strong>Copyright &copy; 2024 <a href="https://www.sourcecodester.com/">VINTAGE FROM RCA-Y1A</a></strong>
+</footer>
